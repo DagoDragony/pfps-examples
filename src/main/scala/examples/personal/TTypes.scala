ae004a216e7219c6c85defa3b999955b4ec35a98
DagoDragony <dago.dragony@gmail.com>
2022-11-19 12:06:58 +0200

    example of TTypes

---
+++ src/main/scala/examples/personal/TTypes.scala
@@ -0,0 +1,22 @@
+package examples.personal
+
+import cats.data.EitherT
+
+import scala.concurrent.Future
+
+object TTypes {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val a: Future[Either[String, Int]] = ???
+  def b(value: Int): Future[Either[String, Int]] = ???
+
+  // doesn't work as a is Either, not a
+  //  for {
+  //    a1 <- a
+  //    b1 <- b(a1)
+  //  } yield ()
+
+  val t2: EitherT[Future, String, Unit] = for {
+    a1 <- EitherT(a)
+    b1 <- EitherT(b(a1))
+  } yield ()
+}
